747d426800d934184105bfeab32f7fee68408630
Allan Taylor <allan.taylor.pi@gmail.com>
2025-03-31 21:04:07 +1300

    Add logos and flesh out header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
   title = 'kiwiangles-nz';
+  navRoutes = routes
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { PlaygroundComponent } from './playground/playground.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home',
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About Us',
+    },
+    {
+      path: 'playground',
+      component: PlaygroundComponent,
+      title: 'Make a word',
+    }
+];
